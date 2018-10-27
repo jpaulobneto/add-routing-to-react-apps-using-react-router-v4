ad552a0baa8a6c2d0a192a08778238501ff10c6b
João Paulo Barbosa <jpaulobneto@gmail.com>
2018-10-27 20:13:18 -0300

    Redirect to Another Page with React Router v4

--- src/components/Links/index.js
+++ src/components/Links/index.js
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom';
 const Links = () => (
   <nav>
     <Link to="/">Home</Link>
-    <Link to="/menu">Menu</Link>
+    <Link to="/old/123">Old</Link>
+    <Link to="/new/456">New</Link>
+    <Link to="/protected">Protected</Link>
   </nav>
 );
 
--- src/components/Root/index.js
+++ src/components/Root/index.js
@@ -1,15 +1,32 @@
 import React, { Fragment } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Redirect, Route } from 'react-router-dom';
 import Links from '../Links';
-import Home from '../../scenes/Home';
-import Menu from '../../scenes/Menu';
+
+const loggedIn = true;
 
 const Root = () => (
   <Router>
     <Fragment>
       <Links />
-      <Route exact path="/" component={Home} />
-      <Route path="/menu" component={Menu} />
+      <Route exact path="/" render={() => <h1>Home</h1>} />
+      <Route
+        path="/new/:str"
+        render={({ match }) => (
+          <h1>
+            New:
+            {` ${match.params.str}`}
+          </h1>
+        )}
+      />
+      <Route
+        path="/old/:str"
+        render={({ match }) => <Redirect to={`/new/${match.params.str}`} />}
+      />
+      <Route
+        path="/protected"
+        render={() => (loggedIn ? <h1>Welcome to the protected page</h1> : <Redirect to="/new/login" />)
+        }
+      />
     </Fragment>
   </Router>
 );
